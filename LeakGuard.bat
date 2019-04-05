77f5106ee7bf48874960890eff9eb5f9cbc139fb
FrostbyteGR <FrostbyteGR@users.noreply.github.com>
2019-04-05 18:00:02 +0300

    Code style changes and fixes
    Fixed unused variables.
    TASKKILL now uses the force.

--- LeakGuard.bat
+++ LeakGuard.bat
@@ -2,7 +2,7 @@
 SETLOCAL EnableDelayedExpansion
 REM ############################################################################
 REM # Batch script to monitor the memory consumption of a given process        #
-REM # and terminate it should it surpass the specified hard limit in GB        #
+REM # and terminate it should the specified limit be surprassed                #
 REM # Copyright (c) 2018 Frostbyte <frostbytegr@gmail.com>                     #
 REM #                                                                          #
 REM # This program is free software: you can redistribute it and/or modify     #
@@ -20,11 +20,11 @@ REM # along with this program.  If not, see <http://www.gnu.org/licenses/>.    #
 REM ############################################################################
 
 REM Settings
-REM Executable name to monitor (file extension must be included):
+REM Process name to monitor (file extension must be included):
 SET processToMonitor=PoorlyWrittenApp.exe
-REM Memory hard limit in GB:
+REM Process memory limit (in GB) to kill process at:
 SET processMemoryLimit=10
-REM Percentage you want to be warned at:
+REM Percentage of process memory limit to issue a warning at:
 SET warningThreshold=90
 
 
@@ -35,62 +35,61 @@ REM ##################################################
 REM ## If you do so, author might lose his religion ##
 REM ##################################################
 
+REM Adjust the process memory limit and warning threshold to KB units.
+SET /A processMemoryLimit=%processMemoryLimit%*1048576
+SET /A processMemoryWarning=%processMemoryLimit%*%warningThreshold%/100
+
 :START
 REM If the requested process is running, proceed with the checks.
-FOR /F %%F IN ('tasklist /NH /FI "IMAGENAME EQ %processToMonitor%"') DO IF %%F == %processToMonitor% GOTO :CHECK
-REM Otherwise, throw error and continue loop.
+FOR /F %%F IN ('TASKLIST /NH /FI "IMAGENAME EQ %processToMonitor%"') DO IF %%F == %processToMonitor% GOTO :CHECK
+REM Otherwise, wait for the requested process to launch.
 CLS
 COLOR 67
-CALL :notify "Process %processToMonitor% is not currently running."
-CALL :notify "(CTRL+C to quit)"
-GOTO :END
+CALL :NOTIFY "Process %processToMonitor% is not currently running."
+CALL :NOTIFY "(CTRL+C to quit)"
+GOTO :LOOP
 
 :CHECK
-REM Pull the working set (KB) of the requested process.
-FOR /F "tokens=*" %%F IN ('tasklist^|findstr %processToMonitor%') DO SET taskDetails=%%F
-FOR /F "tokens=5" %%F IN ("%taskDetails%") DO SET memoryConsumption=%%F
-SET /A memoryConsumption=%memoryConsumption:,=%
+REM Fetch the working set of the requested process (in KB).
+FOR /F "tokens=*" %%F IN ('TASKLIST^|FINDSTR %processToMonitor%') DO SET taskDetails=%%F
+FOR /F "tokens=5" %%F IN ("%taskDetails%") DO SET processMemoryConsumption=%%F
+SET /A processMemoryConsumption=%processMemoryConsumption:,=%
 
-REM Adjust the memory hard limit and the corresponding warning threshold to KB units.
-SET /A memoryLimit=%processMemoryLimit%*1048576
-SET /A memoryWarning=%memoryLimit%*%warningThreshold%/100
-
-REM If memory consumption reached or surpassed the hard limit..
-IF %memoryConsumption% GEQ %memoryLimit% (
-	REM Terminate the process, throw error and continue loop.
-	TASKKILL /IM %processToMonitor%
+REM If current process memory consumption has reached or surpassed the limit..
+IF %processMemoryConsumption% GEQ %processMemoryLimit% (
+	REM Terminate the requested process, throw error and continue the loop.
+	TASKKILL /IM %processToMonitor% /F
 	CLS
 	COLOR 47
-	CALL :notify "Process %processToMonitor% was killed because it exceeded the %processMemoryLimit% GB memory limit."
-	CALL :notify "(CTRL+C to quit)"
-	GOTO :END
+	CALL :NOTIFY "Process %processToMonitor% has been killed, because it's memory consumption exceeded the %processMemoryLimit%KB memory limit."
+	CALL :NOTIFY "(CTRL+C to quit)"
+	GOTO :LOOP
 )
 
-REM If memory consumption reached or surpassed the warning threshold..
-IF %memoryConsumption% GEQ %memoryWarning% (
-	REM Throw warning and continue loop.
+REM If current process memory consumption has reached or surpassed the warning threshold..
+IF %processMemoryConsumption% GEQ %processMemoryWarning% (
+	REM Throw warning and continue the loop.
 	CLS
 	COLOR 67
-	CALL :notify "Process %processToMonitor% has exceeded %warningThresholdPercent%% (%memoryConsumption% KB) of it's allotted memory."
-	CALL :notify "(CTRL+C to quit)"
-	GOTO :END
+	CALL :NOTIFY "Process %processToMonitor% memory consumption has exceeded the %processMemoryWarning%KB (%warningThreshold% Percent) warning threshold."
+	CALL :NOTIFY "(CTRL+C to quit)"
+	GOTO :LOOP
 )
 
-REM Notify about current memory consumption.
+REM Notify about current process memory consumption.
 CLS
 COLOR 27
-CALL :notify "Process %processToMonitor% is currently within it's allotted memory (%memoryConsumption% KB)."
-CALL :notify "(CTRL+C to quit)"
-GOTO :END
+CALL :NOTIFY "Process %processToMonitor% memory consumption is currently within the limit (%processMemoryConsumption%/%processMemoryLimit%KB)."
+CALL :NOTIFY "(CTRL+C to quit)"
+GOTO :LOOP
 
-REM Loop delay of 10 seconds.
-:END
+REM Loop controller - 10 seconds of delay between cycles.
+:LOOP
 PING -n 9 127.0.0.1 >NUL
 GOTO :START
-GOTO :EOF
 
 REM Notification function - Input will be printed out to console.
-:notify
+:NOTIFY
 SETLOCAL
 ECHO %~1
 ENDLOCAL
